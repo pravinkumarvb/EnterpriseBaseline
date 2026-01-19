9c43866b176a3e705bc384e75c0c7cc0673373a0
pravinkumarvb <pravinkumar.v.birajdar@gmail.com>
2026-01-19 20:36:26 +0530

    chore(api): register AppDbContext with SQL Server

--- EnterpriseBaseline.Api/Program.cs
+++ EnterpriseBaseline.Api/Program.cs
@@ -1,9 +1,19 @@
+using EnterpriseBaseline.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Register DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
